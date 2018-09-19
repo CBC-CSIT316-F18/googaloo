e0325c8d59414ffd79e87e716ed5ea822f47b480
CS550 <CS550@columbiabasin.edu>
2018-09-19 10:41:30 -0700

    initial commit

---
+++ header.php
@@ -0,0 +1,23 @@
+<?php
+
+function printheader()
+{
+    print("<html>");
+    
+    print("<head>
+        <title>TODO supply a title</title>
+        <meta charset=\"UTF-8\">
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+    print("<link rel=\"stylesheet\" type=\"text/css\" href=\"egstylesheet.css\">");
+    print("</head>"); 
+    print("<body>");
+    print("<div id=\"idheader\"></div>");
+}
+
+
+
+function printfooter()
+{
+    print("</body>");  
+print("</html>");
+}
