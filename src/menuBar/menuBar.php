d3131f46135767929e0d53df94e00ec7c66fc5f9
Tycko Franklin <Tycko.franklin@students.columbiabasin.edu>
2018-09-19 23:55:30 -0700

    Created a menu bar, a few folders, and moved a file or two

---
+++ src/menuBar/menuBar.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TyckoFranklin
+ * Date: 9/19/2018
+ * Time: 11:10 PM
+ */
+
+/**
+ * @param $menuTitle string
+ */
+function printMenuBar($menuTitle){
+    // put in some dummy data
+    $menuItems = [
+        [
+            'href' => 'www.google.com',
+            'name' => 'Google',
+        ],
+        [
+            'href' => 'www.columbiabasin.edu',
+            'name' => 'CBC',
+        ],
+    ];
+
+    /*  start the menu bar  */
+    print('<div class="mainMenu">');
+
+    print('<div class="menuTitle">'.$menuTitle.'</div>');
+
+    print('<div class="menuLinks">');
+    /* fill the menu bar  */
+    printMenuItem($menuItems);
+    print('</div>');
+
+    /*  end the menu bar  */
+    print('</div>');
+
+
+}
+
+function printMenuItem($menuItems){
+
+    foreach($menuItems as $links){
+        print('<div class="menuItem"><a href="'
+            .$links["href"]
+            .'">'.$links["name"]
+            .'</a>'
+            .'</div>'
+        );
+    }
+
+}
