4785fcc72682075d2549d8c7dcbc3365bc9cb249
CS550 <realityfabric@gmail.com>
2018-09-26 11:00:58 -0700

    Add script for db creation

---
+++ createDB.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE charter;
+
+USE charter;
+
+CREATE TABLE users (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	`type` INT,
+	username VARCHAR(30),
+	email VARCHAR(80),
+	pass VARBINARY(32),
+	first_name VARCHAR(20),
+	last_name VARCHAR(40),
+	date_expires DATE,
+	date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	date_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
