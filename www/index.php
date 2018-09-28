47a72f921bf7866f0042394d75110189d5c22295
Tycko Franklin <Tycko.franklin@students.columbiabasin.edu>
2018-09-28 05:48:42 -0700

    something to do

--- www/index.php
+++ www/index.php
@@ -1,30 +1,31 @@
 <?php
 include 'db_tools.php';
 include "header.php";
+include "src/body/body.php";
 
 printheader();
+printBody();
 
 
-
-$db=new db_tools;
-$db->db_connect();
-$sql = "SELECT * FROM `users`";
-$rows=$db->db_select($sql);
-
-$ilength = count($rows);
-for($i = 0; $i < $ilength; $i++) 
-{
-    $jlength = count($rows[$i]);
-    $row=$rows[$i];
-   for($j = 0; $j < $jlength; $j++) 
-    {    
-        echo $row['ID'];
-         echo $row['first_name'];
-          echo $row['last_name'];
-           echo $row['email'];
-            echo $row['username'];
-        echo "<br>";
-    }
-}
+//$db=new db_tools;
+//$db->db_connect();
+//$sql = "SELECT * FROM `users`";
+//$rows=$db->db_select($sql);
+//
+//$ilength = count($rows);
+//for($i = 0; $i < $ilength; $i++)
+//{
+//    $jlength = count($rows[$i]);
+//    $row=$rows[$i];
+//   for($j = 0; $j < $jlength; $j++)
+//    {
+//        echo $row['ID'];
+//         echo $row['first_name'];
+//          echo $row['last_name'];
+//           echo $row['email'];
+//            echo $row['username'];
+//        echo "<br>";
+//    }
+//}
 
 printfooter();
---
+++ www/src/body/body.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TyckoFranklin
+ * Date: 9/27/2018
+ * Time: 9:53 PM
+ */
+use src\util\html\ElementBuilder\ElementBuilder;
+function printBody()
+{
+
+    $ourBodyBuilder = ElementBuilder::create("div")
+        ->withAttribute("class", "app")
+        ->withTextContent("This is a test of the builder!!!")
+        ->buildLeaveTagOpen();
+
+
+    $ourChildBodyBuilder = ElementBuilder::create("div")
+        ->withAttribute("class", "appChild")
+        ->withTextContent("More Text Here")
+        ->buildLeaveTagOpen()
+        ->buildCloseOpenTag();
+
+    $ourBodyBuilder->buildCloseOpenTag();
+}
