e708ff339ec6d2b22248545a4d9a3a6a70d35311
Jessica Fairchild <realityfabric@gmail.com>
2018-09-27 21:49:59 -0700

    Reorganize for better readability
    In the future it will also be easier to see which fields exactly have been added or removed, and who did it.

--- createUser.sql
+++ createUser.sql
@@ -1,8 +1,26 @@
 /* Insert User Code */
 
-INSERT INTO `users` (`type`, `username`, `email`, `pass`, `first_name`, `last_name`, `date_expires`, `date_created`, `date_modified`)
-VALUES 
-('administrator', 'username', 'user@email.com',0x70617373776f7264, 'FirstName', 'LastName', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO `users` (
+  `type`,
+  `username`,
+  `email`,
+  `pass`,
+  `first_name`,
+  `last_name`,
+  `date_expires`,
+  `date_created`,
+  `date_modified`
+) VALUES (
+  'administrator',
+  'username',
+  'user@email.com',
+  0x70617373776f7264,
+  'FirstName',
+  'LastName',
+  NULL,
+  CURRENT_TIMESTAMP,
+  CURRENT_TIMESTAMP
+);
 
 /* Insert Basic User Example */
 
