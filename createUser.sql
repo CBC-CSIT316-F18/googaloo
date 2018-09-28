aa461fbffc25c70b8e95ad1d293c31edbcfa71cf
Jessica Fairchild <realityfabric@gmail.com>
2018-09-27 21:47:18 -0700

    Add example for inserting a standard user
    This leaves out all of the fields which are set automatically (and the date_expires, which should automatically become NULL).

--- createUser.sql
+++ createUser.sql
@@ -3,3 +3,19 @@
 INSERT INTO `user` (`type`, `username`, `email`, `pass`, `first_name`, `last_name`, `date_expires`, `date_created`, `date_modified`)
 VALUES 
 ('administrator', 'username', 'user@email.com',0x70617373776f7264, 'FirstName', 'LastName', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+/* Insert Basic User Example */
+
+INSERT INTO `users` (
+  `username`,
+  `email`,
+  `pass`,
+  `first_name`,
+  `last_name`
+) VALUES (
+  'standarduser',
+  'standard@mail.com',
+  0x70617373776f7264,
+  'Stan',
+  'Dard'
+)
