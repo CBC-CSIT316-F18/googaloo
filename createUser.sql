b9bf5749f2ba72f73800d1bcaed65acc1a8b6ae3
Jessica Fairchild <realityfabric@gmail.com>
2018-09-27 21:47:40 -0700

    Fix table name in INSERT statement

--- createUser.sql
+++ createUser.sql
@@ -1,6 +1,6 @@
 /* Insert User Code */
 
-INSERT INTO `user` (`type`, `username`, `email`, `pass`, `first_name`, `last_name`, `date_expires`, `date_created`, `date_modified`)
+INSERT INTO `users` (`type`, `username`, `email`, `pass`, `first_name`, `last_name`, `date_expires`, `date_created`, `date_modified`)
 VALUES 
 ('administrator', 'username', 'user@email.com',0x70617373776f7264, 'FirstName', 'LastName', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
